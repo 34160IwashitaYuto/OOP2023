56ceadc8860e53f816eeb63d64714459594d3123
infosys <infosys@C4222>
2023-04-17 10:07:55 +0900

    プロジェクト ファイルを追加します。

---
+++ Sample/ConsoleApp0411/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp0411 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("合計金額:");//金額入力
+            int money = int.Parse(Console.ReadLine());
+
+            Console.Write("支払い:");//金額入力
+            int hpaymentar = int.Parse(Console.ReadLine());
+
+            int change = hpaymentar - money;//お釣り
+            int Ten_thousand = 0;
+            int Five_thousand = 0;
+            int Two_thousand = 0;
+            int One_thousand = 0;
+            int Five_hudred = 0;
+            int One_hudred = 0;
+            int goju = 0;
+            int Ten = 0;
+            int Five = 0;
+            int one = 0;
+
+            Console.WriteLine("お釣り:" + change);
+
+            while (change >= 10000)
+            {
+                change -= 10000;
+                Ten_thousand = Ten_thousand + 1;
+            }
+
+            while (change >= 5000)
+            {
+                change -= 5000;
+                Five_thousand = Five_thousand + 1;
+            }
+
+            while (change >= 2000)
+            {
+                change -= 2000;
+                Two_thousand = Two_thousand + 1;
+            }
+
+            while (change >= 1000)
+            {
+                change -= 1000;
+                One_thousand = One_thousand + 1;
+            }
+
+            while (change >= 500)
+            {
+                change -= 500;
+                Five_hudred = Five_hudred + 1;
+            }
+
+            while (change >= 100)
+            {
+                change -= 100;
+                One_hudred = One_hudred + 1;
+            }
+
+            while (change >= 50)
+            {
+                change -= 50;
+                goju = goju + 1;
+            }
+
+            while (change >= 10)
+            {
+                change -= 10;
+                Ten = Ten + 1;
+            }
+
+            while (change >= 5)
+            {
+                change -= 5;
+                Five = Five + 1;
+            }
+
+            while (change >= 1)
+            {
+                change -= 1;
+                one = one + 1;
+            }
+
+            /*
+            Console.WriteLine("一万円札:" + Ten_thousand + "枚");
+            Console.WriteLine("五千円札:" + Five_thousand + "枚");
+            Console.WriteLine("二千円札:" + Two_thousand + "枚");
+            Console.WriteLine("千円札:" + One_thousand + "枚");
+            Console.WriteLine("五百円玉:" + Five_hudred + "枚");
+            Console.WriteLine("百円玉:" + One_hudred + "枚");
+            Console.WriteLine("五十円玉:" + goju + "枚");
+            Console.WriteLine("十円玉:" + Ten + "枚");
+            Console.WriteLine("五円玉:" + Five + "枚");
+            Console.WriteLine("一円玉:" + one + "枚");
+            */
+
+            
+
+        }
+
+        private static void asOut(int count) {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Sample/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace WindowsFormsApp1 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.btbutton = new System.Windows.Forms.Button();
+            this.tbNum2 = new System.Windows.Forms.TextBox();
+            this.tbNum1 = new System.Windows.Forms.TextBox();
+            this.tbAncer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btbutton
+            // 
+            this.btbutton.Location = new System.Drawing.Point(616, 145);
+            this.btbutton.Name = "btbutton";
+            this.btbutton.Size = new System.Drawing.Size(144, 60);
+            this.btbutton.TabIndex = 0;
+            this.btbutton.Text = "計算";
+            this.btbutton.UseVisualStyleBackColor = true;
+            this.btbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbNum2
+            // 
+            this.tbNum2.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbNum2.Location = new System.Drawing.Point(367, 43);
+            this.tbNum2.Name = "tbNum2";
+            this.tbNum2.Size = new System.Drawing.Size(144, 34);
+            this.tbNum2.TabIndex = 1;
+            // 
+            // tbNum1
+            // 
+            this.tbNum1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbNum1.Location = new System.Drawing.Point(134, 43);
+            this.tbNum1.Name = "tbNum1";
+            this.tbNum1.Size = new System.Drawing.Size(144, 34);
+            this.tbNum1.TabIndex = 1;
+            // 
+            // tbAncer
+            // 
+            this.tbAncer.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbAncer.Location = new System.Drawing.Point(616, 43);
+            this.tbAncer.Name = "tbAncer";
+            this.tbAncer.Size = new System.Drawing.Size(144, 34);
+            this.tbAncer.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(316, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "+\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(560, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 278);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbNum1);
+            this.Controls.Add(this.tbAncer);
+            this.Controls.Add(this.tbNum2);
+            this.Controls.Add(this.btbutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btbutton;
+        private System.Windows.Forms.TextBox tbNum2;
+        private System.Windows.Forms.TextBox tbNum1;
+        private System.Windows.Forms.TextBox tbAncer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
