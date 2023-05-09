791ed5aaf093378e3327d81f4c169abbfd315d26
岩下　優斗 <OIBC34160@ojs.ac.jp>
2023-05-09 09:43:24 +0900

    0508演習2

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -18,18 +18,37 @@ namespace ProductSample {
             //Console.WriteLine("大福もちの税込み価格:" + daifukumoti.GetPriceIncludingTax());
             #endregion
 
+            #region 
             //DateTime date = new DateTime(2023,5,8);
-            DateTime date = DateTime.Today;
+            //DateTime date = DateTime.Today;
 
             //10日後を求める
-            DateTime daysAfter10 = date.AddDays(10);
+            //DateTime daysAfter10 = date.AddDays(10);
 
             //10日前を求める
-            DateTime daysformer10 = date.AddDays(-10);
+            //DateTime daysformer10 = date.AddDays(-10);
 
-            Console.WriteLine("今日の日付:" +date.Year+"年"+ date.Month+"月"+ date.Day + "日です");
-            Console.WriteLine("10日後:" + daysAfter10.Year + "年" + daysAfter10.Month + "月"+ daysAfter10.Day + "日です");
-            Console.WriteLine("10日前:" + daysformer10.Year + "年" + daysformer10.Month + "月" + daysformer10.Day + "日です");
+            //Console.WriteLine("今日の日付:" +date.Year+"年"+ date.Month+"月"+ date.Day + "日です");
+            //Console.WriteLine("10日後:" + daysAfter10.Year + "年" + daysAfter10.Month + "月"+ daysAfter10.Day + "日です");
+            //Console.WriteLine("10日前:" + daysformer10.Year + "年" + daysformer10.Month + "月" + daysformer10.Day + "日です");
+            #endregion
+
+
+
+
+            Console.WriteLine("誕生日を入力");
+            Console.Write("西暦:");
+            int Anno_Domini = int.Parse(Console.ReadLine());
+            Console.Write("月:");
+            int Month = int.Parse(Console.ReadLine());
+            Console.Write("日:");
+            int Day = int.Parse(Console.ReadLine());
+
+            DateTime date = DateTime.Today;
+            DateTime barthday = new DateTime(Anno_Domini, Month, Day);
+
+            Console.Write(date - barthday);
+
 
         }
     }
