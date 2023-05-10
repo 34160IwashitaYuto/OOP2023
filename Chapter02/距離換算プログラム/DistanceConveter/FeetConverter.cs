fb59387600f2e50ed53f0bc2efed397da4c462a1
岩下　優斗 <OIBC34160@ojs.ac.jp>
2023-05-10 11:25:17 +0900

    距離換算プログラム完成

--- Chapter02/距離換算プログラム/DistanceConveter/FeetConverter.cs
+++ Chapter02/距離換算プログラム/DistanceConveter/FeetConverter.cs
@@ -7,16 +7,20 @@ using System.Threading.Tasks;
 namespace DistanceConveter {
     //フィートとメートルの単位変換クラス
     public static class FeetConverter {
+
+        //定数
+        private const double ratio = 0.3048;
         
+
         //メートルからフィート
         public static double FromMeter(int meter) {
-            return meter / 0.3048;
+            return meter / ratio;
 
         }
 
         //フィートからメートルを求める
         public static double ToMeter(int feet) {
-            return feet * 0.3048;
+            return feet * ratio;
         }
 
     }
