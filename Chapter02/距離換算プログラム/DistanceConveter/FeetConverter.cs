7f318e64748e7e9cff687f92469e068c4e418a56
岩下　優斗 <OIBC34160@ojs.ac.jp>
2023-05-10 10:24:58 +0900

    メソッドを単機能

---
+++ Chapter02/距離換算プログラム/DistanceConveter/FeetConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConveter {
+    //フィートとメートルの単位変換クラス
+    public class FeetConverter {
+        
+        //メートルからフィート
+        public static double FromMeter(int meter) {
+            return meter / 0.3048;
+
+        }
+
+        //フィートからメートルを求める
+        public double ToMeter(int feet) {
+            return feet * 0.3048;
+        }
+
+    }
+}
