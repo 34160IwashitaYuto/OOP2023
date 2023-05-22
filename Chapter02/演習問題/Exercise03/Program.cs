5c59e5b00964f5cb20191c1de272b023c16e9e85
岩下　優斗 <OIBC34160@ojs.ac.jp>
2023-05-22 10:33:04 +0900

    問題2.3完成

--- Chapter02/演習問題/Exercise03/Program.cs
+++ Chapter02/演習問題/Exercise03/Program.cs
@@ -7,11 +7,12 @@ using System.Threading.Tasks;
 namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
-
-
-
-
-
+            var sales = new SalesCounter(@"data\sales.csv");
+            var amountPerStor = sales.GetPerStoreSales();
+            foreach (var obj in amountPerStor)
+            {
+                Console.WriteLine("{0}{1:C}", obj.Key, obj.Value);
+            }
         }
     }
 }
