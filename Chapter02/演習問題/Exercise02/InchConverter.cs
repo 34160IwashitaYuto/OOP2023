c696168b03b24f64f8a69617ca0943baae63c866
岩下　優斗 <OIBC34160@ojs.ac.jp>
2023-05-22 09:29:48 +0900

    P79問題2.2完成

---
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    //フィートとメートルの単位変換クラス
+    public static class InchConverter {
+
+        //定数
+        private const double ratio = 0.0254;
+        
+
+        //メートルからフィート
+        public static double FromMeter(int meter) {
+            return meter / ratio;
+
+        }
+
+        //フィートからメートルを求める
+        public static double ToMeter(int feet) {
+            return feet * ratio;
+        }
+
+    }
+}
