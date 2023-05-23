378421ce9bf3751eaed54b1da81ea5fadc28e10e
岩下　優斗 <OIBC34160@ojs.ac.jp>
2023-05-23 10:30:32 +0900

    P87まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -9,11 +9,18 @@ namespace Section01 {
         public delegate bool Judgement(int value);  //デリゲートの宣言
 
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4};
 
-            int count = numbers.Count(n => n % 2 == 0);
+            //5の倍数をカウントする
+            //int count = numbers.Count(n => n > 0 && n % 5 == 0);
 
-            Console.WriteLine(count);
+            //合計値
+            //var sum = numbers.Sum(n => n);
+
+            //偶数の平均値
+            var sum = numbers.Where(n => n % 2 == 0).Average();
+
+            Console.WriteLine(sum);
         }
     }
 }
