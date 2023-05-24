5652a83eb5ffafb7b4c88b75b0b5bf03cfeb83f5
岩下　優斗 <OIBC34160@ojs.ac.jp>
2023-05-24 10:11:26 +0900

    演習3.1完成

--- Chapter03/演習問題/Exercise01/Program.cs
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -12,8 +12,29 @@ namespace Exercise01 {
 
             // 3.1.1
             Exercise1_1(numbers);
+            
+            Console.WriteLine("-----");
+
+
+            // 3.1.2
+            Exercise1_2(numbers);
+            
+            Console.WriteLine("-----");
+
+            // 3.1.3
+            Exercise1_3(numbers);
+            Console.WriteLine("-----");
+
+
+            // 3.1.4
+            Exercise1_4(numbers);
+            Console.WriteLine("-----");
+
+        }
+
+        private static void Exercise1_1(List<int> numbers) {
             var exists = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
-            if(exists)
+            if (exists)
             {
                 Console.WriteLine("存在します");
             }
@@ -21,32 +42,22 @@ namespace Exercise01 {
             {
                 Console.WriteLine("存在しません");
             }
-            
-
-            // 3.1.2
-            Exercise1_2(numbers);
-            var quotient = numbers.ConvertAll(n => n / 2.0);
-            quotient.ForEach(s => Console.WriteLine(s));
-            Console.WriteLine("-----");
-
-            // 3.1.3
-            Exercise1_3(numbers);
-            Console.WriteLine("-----");
-
-            // 3.1.4
-            Exercise1_4(numbers);
-        }
-
-        private static void Exercise1_1(List<int> numbers) {
         }
 
         private static void Exercise1_2(List<int> numbers) {
+            numbers.ForEach(s => Console.WriteLine(s / 2.0));
         }
 
         private static void Exercise1_3(List<int> numbers) {
+            (numbers.Where(n => n >= 50).ToList()).ForEach(s => Console.WriteLine(s + ""));
         }
 
         private static void Exercise1_4(List<int> numbers) {
+            List<int> list = numbers.Select(n => n * 2).ToList();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item + "");
+            }
         }
     }
 }
