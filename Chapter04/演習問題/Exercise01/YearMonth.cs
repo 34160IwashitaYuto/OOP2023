5ab19d6b6d3dee4a8e315720a6462d1c2671dc0a
岩下　優斗 <OIBC34160@ojs.ac.jp>
2023-05-30 11:23:56 +0900

    4.1.2

---
+++ Chapter04/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    public class YearMonth {
+        //4.1.1
+        public int Year { get; private set; }
+
+        public int Month { get; private set;}
+
+        public YearMonth(int year,int month) {
+            Year = year;
+            Month = month;
+        }
+
+        //4.1.2
+        public bool Is21Century
+        {
+            get
+            {
+                return Year <= 2100 || Year >= 2001;
+            }
+        }
+
+
+
+
+        //4.1.3
+        public YearMonth AddOneMonth() {
+
+        }
+
+
+        //4.1.4
+        //public override string ToString() {
+            
+        //}
+
+    }
+}
---
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -0,0 +1,16 @@
+﻿using Exercise01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+
+            var ym = new YearMonth(2023, 5);
+            bool c21 = ym.Is21Century;
+        }
+    }
+}
