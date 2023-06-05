a55bf0b790d24d2998abb1c45b2f0a136a59b70e
岩下　優斗 <OIBC34160@ojs.ac.jp>
2023-06-05 09:42:23 +0900

    3_2完成

--- Chapter05/演習問題/Exercise03/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Exercise03/Program.cs
@@ -7,6 +7,45 @@ using System.Threading.Tasks;
 namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
+            var text = "Jackdaws love my big sphinx of quartz";
+
+            Exercise3_1(text);
+            Console.WriteLine("-----");
+
+            Exercise3_2(text);
+            Console.WriteLine("-----");
+
+            Exercise3_3(text);
+            Console.WriteLine("-----");
+
+            Exercise3_4(text);
+            Console.WriteLine("-----");
+
+            Exercise3_5(text);
+        }
+
+
+        private static void Exercise3_1(string text) {
+            var contains = text.Contains(" ");
+            Console.WriteLine(contains);
+
+        }
+
+        private static void Exercise3_2(string text) {
+            var replaced = text.Replace("big","small");
+            Console.WriteLine(replaced);
+        }
+
+        private static void Exercise3_3(string text) {
+            
+        }
+
+        private static void Exercise3_4(string text) {
+            
+        }
+
+        private static void Exercise3_5(string text) {
+            
         }
     }
 }
