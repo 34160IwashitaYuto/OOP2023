1e99f9e7e385cb364ae4e12be68ab72f1a6ebefc
岩下　優斗 <OIBC34160@ojs.ac.jp>
2023-06-06 10:12:46 +0900

    演習5_4

---
+++ Chapter05/演習問題/Exercise04/Exercise04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise04 {
+    class Program {
+        static void Main(string[] args) {
+            var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+            var novelist = line.Substring(9,5);
+            Console.WriteLine("作家:"+novelist);
+            var bestwork = line.Substring(24,3);
+            Console.WriteLine("代表作:" + bestwork);
+            var born = line.Substring(33,4);
+            Console.WriteLine("誕生年:" + born);
+        }
+    }
+}
