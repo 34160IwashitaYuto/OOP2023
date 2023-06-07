f7fb9b532be303eb9d0a46a1283fe33e315321ca
岩下　優斗 <OIBC34160@ojs.ac.jp>
2023-06-07 10:53:18 +0900

    LINQのメモリ使用量

--- Chapter05/演習問題/Exercise04/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Exercise04/Program.cs
@@ -7,13 +7,53 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
+#if Array
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-            var novelist = line.Substring(9,5);
-            Console.WriteLine("作家:"+novelist);
-            var bestwork = line.Substring(24,3);
-            Console.WriteLine("代表作:" + bestwork);
-            var born = line.Substring(33,4);
-            Console.WriteLine("誕生年:" + born);
+
+
+
+            
+
+#elif StringArray
+            string[] lines = {
+                "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886"
+                "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867"
+                "Novelist=太宰治;BestWork=人間失格;Born=1909"
+                "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896"
+            };
+
+#endif
+
+#if NonArray
+
+#elif StringArray
+            
+            foreach(var line in lines){
+                foreach (var pair in line.Split(';'))
+                {
+                var array = pair.Split('=');
+                Console.WriteLine("{0},{1}", array[0], array[1]);
+                }
+            }
+
+
+#endif
+            Console.WriteLine("実行時間={0}",sw.Elapsed.ToString(@"ss\.fffff"));
+        }
+        static string ToJapanese(string key) {
+                switch (key)
+                {
+                case "NoveList":
+                    return "作家";
+                case "BestWork":
+                    return "代表作";
+                case "Born":
+                    return "誕生年";
+                }
+
+            throw new ArgumentException("正しい引数ではありません");
+
+
         }
     }
 }
---
+++ Chapter06/Section02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section02 {
+    class Program {
+        static void Main(string[] args) {
+            Stopwatch sw = new Stopwatch();//タイマーインスタンス
+            sw.Start();//タイマースタート
+
+            var numbers = Enumerable.Range(1, 1000000).ToArray();
+            WriteTotalMemory("配列確保後");
+
+            //偶数のみ取り出し
+            var evenNumbers = numbers.Where(n=> n % 2 == 0).ToArray();
+
+            WriteTotalMemory("配列抽出後");
+
+
+            //foreach (var item in evenNumbers)
+            //{
+            //    Console.WriteLine(item+" ");
+            //}
+
+
+            var ave = evenNumbers.Average();
+            WriteTotalMemory("偶数抽出後平均");
+            Console.WriteLine("実行時間 = {0}", sw.Elapsed.ToString(@"ss\.fffff"));//時間表示
+
+
+
+        }
+
+        static void WriteTotalMemory(string header) {
+            var totalMemory = GC.GetTotalMemory(true) / 1024.0 / 1024.0;
+            Console.WriteLine($"{header}: {totalMemory:0.000 MB}");
+        }
+    }
+}
