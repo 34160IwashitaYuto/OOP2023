b7c305af4d9f7d3ae7c7fb7ea408d4d06201cfbb
岩下　優斗 <OIBC34160@ojs.ac.jp>
2023-06-30 11:42:03 +0900

    演習8.1

---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            var dateTime = new DateTime(2023, 6, 30, 11, 36, 47);
+            DisplayDatePattern1(dateTime);
+            DisplayDatePattern2(dateTime);
+            DisplayDatePattern3(dateTime);
+            DisplayDatePattern3_2(dateTime);
+
+        }
+
+        private static void DisplayDatePattern1(DateTime dateTime) {
+            var str = string.Format("{0}/{1,2}/{2,2} {3}:{4}", dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute);
+            Console.WriteLine(str);
+        }
+
+        private static void DisplayDatePattern2(DateTime dateTime) {
+            var str = string.Format("{0}年{1,2}月{2,2}日 {3}時{4}分{5}秒", dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute,dateTime.Second);
+            Console.WriteLine(str);
+        }
+
+        private static void DisplayDatePattern3(DateTime dateTime) {
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var era = culture.DateTimeFormat.Calendar.GetEra(dateTime);
+            var eraName = culture.DateTimeFormat.GetEraName(era);
+            var dayOfWeek = culture.DateTimeFormat.GetDayName(dateTime.DayOfWeek);
+            var str = string.Format("{0}年{1,2}月{2,2}日({3})", eraName,dateTime.Month, dateTime.Day, dayOfWeek);
+            Console.WriteLine(str);
+        }
+
+        private static void DisplayDatePattern3_2(DateTime dateTime) {
+            
+        }
+    }
+}
