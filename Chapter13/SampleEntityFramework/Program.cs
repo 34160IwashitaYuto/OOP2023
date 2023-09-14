a46b2d2f7878fcb51f63d065b3c9c751b48c7710
優斗　岩下 <OIBC34160@ojs.ac.jp>
2023-09-14 10:28:56 +0900

    13.1.1修正

--- Chapter13/SampleEntityFramework/Program.cs
+++ Chapter13/SampleEntityFramework/Program.cs
@@ -54,52 +54,68 @@ namespace SampleEntityFramework {
                     Gender = "M",
                     Name = "菊池寛",
                 };
+                db.Authors.Add(author1);
 
                 var author2 = new Author {
                     Birthday = new DateTime(1899, 6, 14),
                     Gender = "M",
                     Name = "川端康成",
                 };
+                db.Authors.Add(author2);
 
                 var book1 = new Book {
                     Title = "こころ",
                     PublishedYear = 1991,
                     Author = new Author {
+                        Birthday = new DateTime(1867, 2, 9),
+                        Gender = "M",
                         Name = "夏目漱石",
                     }
                 };
+                db.Books.Add(book1);
 
                 var book2 = new Book {
                     Title = "伊豆の踊子",
                     PublishedYear = 2003,
                     Author = new Author {
+                        Birthday = new DateTime(1899, 6, 14),
+                        Gender = "M",
                         Name = "川端康成",
                     }
                 };
+                db.Books.Add(book2);
 
                 var book3 = new Book {
                     Title = "真珠夫人",
                     PublishedYear = 2002,
                     Author = new Author {
+                        Birthday = new DateTime(1888, 12, 26),
+                        Gender = "M",
                         Name = "菊池寛",
                     }
                 };
+                db.Books.Add(book3);
 
                 var book4 = new Book {
                     Title = "注文の多い料理店",
                     PublishedYear = 2000,
                     Author = new Author {
+                        Birthday = new DateTime(1896, 8, 27),
+                        Gender = "M",
                         Name = "宮沢賢治",
                     }
                 };
+                db.Books.Add(book4);
+                db.SaveChanges();
 
             }
         }
 
         private static void Exercise1_2() {
+            using (var db = new BooksDbContext()) {
 
+            }
         }
-
         private static void Exercise1_3() {
 
         }
