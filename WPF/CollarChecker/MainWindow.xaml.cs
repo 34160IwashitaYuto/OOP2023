6507c54cc73796c87e33cf1c9c8c26ac4b61f254
優斗　岩下 <OIBC34160@ojs.ac.jp>
2023-11-01 12:02:03 +0900

    stock途中

--- WPF/CollarChecker/MainWindow.xaml.cs
+++ WPF/CollarChecker/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,6 +21,50 @@ namespace CollarChecker {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            DataContext = GetColorList();
         }
+
+
+        /// <summary>
+        /// すべての色を取得するメソッド
+        /// </summary>
+        /// <returns></returns>
+        private MyColor[] GetColorList() {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                    .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
+        }
+
+
+        /// <summary>
+        /// 色と色名を保持するクラス
+        /// </summary>
+        public class MyColor {
+            public Color Color { get; set; }
+            public string Name { get; set; }
+            public override string ToString() {
+                return "R:" + Color.R + "G:" + Color.G + "B:" + Color.B;
+            }
+        }
+
+
+        private void SampleSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            colorArer.Background = new SolidColorBrush(Color.FromRgb((byte)rSampleSlider.Value,
+                                            (byte)gSampleSlider.Value, (byte)bSampleSlider.Value));
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            ListBox.Items.Add(new MyColor {
+                Color = Color.FromRgb((byte)rSampleSlider.Value,
+                                      (byte)gSampleSlider.Value,
+                                      (byte)bSampleSlider.Value)
+            });
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            
+
+        }
+
     }
 }
